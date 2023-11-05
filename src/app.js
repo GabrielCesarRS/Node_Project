8d889d1216deccdc2c2cc39536d2ae01e91d3398
GabrielCesarRS <gabrielcrs22@gmail.com>
2023-11-04 22:34:31 -0300

    feat: inseri o pre-commit, fazendo as mudanças necessárias antes de commitar

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/app', function (req, res) {
+  res.send('Olá, ta ok');
+});
+app.get('/produtos/:nome/categoria/:categoria/valor/:valor', (req, res) => {
+  res.send({
+    nome: req.params.nome,
+    categoria: req.params.categoria,
+    valor: req.params.valor,
+  });
+});
+
+app.delete('/produtos/camisa', (req, res) => {
+  res.send('Camisa cabo po kkkk <br> ENDPOINT da camisa via postman');
+});
+app.listen(3333, function () {
+  console.log('TA TUDO OK NA PORTA 3333');
+});
